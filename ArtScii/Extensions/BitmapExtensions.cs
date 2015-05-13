03558f17e0dcbff66a7b808e6adebd5db5212be6
yankejustin <yankejustin@hotmail.com>
2015-05-13 14:07:35 -0400

    Initial Push
    Initial push of the project. Needs to be adapted to work with the user
    interface and needs more user-interface interaction. Could also use some
    more formatting.

---
+++ ArtScii/Extensions/BitmapExtensions.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace ArtScii.Extensions
+{
+    public static class BitmapExtensions
+    {
+        public static string ASCIIFilter(this Bitmap sourceBitmap, int pixelBlockSize,
+                                                                   int colorCount = 0)
+        {
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                    sourceBitmap.Width, sourceBitmap.Height),
+                                                      ImageLockMode.ReadOnly,
+                                                PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            StringBuilder asciiArt = new StringBuilder();
+
+            int avgBlue = 0;
+            int avgGreen = 0;
+            int avgRed = 0;
+            int offset = 0;
+
+            int rows = sourceBitmap.Height / pixelBlockSize;
+            int columns = sourceBitmap.Width / pixelBlockSize;
+
+            if (colorCount > 0)
+            {
+                colorCharacters = GenerateRandomString(colorCount);
+            }
+
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < columns; x++)
+                {
+                    avgBlue = 0;
+                    avgGreen = 0;
+                    avgRed = 0;
+
+                    for (int pY = 0; pY < pixelBlockSize; pY++)
+                    {
+                        for (int pX = 0; pX < pixelBlockSize; pX++)
+                        {
+                            offset = y * pixelBlockSize * sourceData.Stride +
+                                     x * pixelBlockSize * 4;
+
+                            offset += pY * sourceData.Stride;
+                            offset += pX * 4;
+
+                            avgBlue += pixelBuffer[offset];
+                            avgGreen += pixelBuffer[offset + 1];
+                            avgRed += pixelBuffer[offset + 2];
+                        }
+                    }
+
+                    avgBlue = avgBlue / (pixelBlockSize * pixelBlockSize);
+                    avgGreen = avgGreen / (pixelBlockSize * pixelBlockSize);
+                    avgRed = avgRed / (pixelBlockSize * pixelBlockSize);
+
+                    asciiArt.Append(GetColorCharacter(avgBlue, avgGreen, avgRed));
+                }
+
+                asciiArt.Append("\r\n");
+            }
+
+            return asciiArt.ToString();
+        }
+
+        private static string GenerateRandomString(int maxSize)
+        {
+            StringBuilder stringBuilder = new StringBuilder(maxSize);
+            Random randomChar = new Random();
+
+            char charValue;
+
+            for (int k = 0; k < maxSize; k++)
+            {
+                charValue = (char)(Math.Floor(255 * randomChar.NextDouble() * 4));
+
+                if (stringBuilder.ToString().IndexOf(charValue) != -1)
+                {
+                    charValue = (char)(Math.Floor((byte)charValue *
+                                            randomChar.NextDouble()));
+                }
+
+                if (Char.IsControl(charValue) == false &&
+                    Char.IsPunctuation(charValue) == false &&
+                    stringBuilder.ToString().IndexOf(charValue) == -1)
+                {
+                    stringBuilder.Append(charValue);
+
+                    randomChar = new Random((int)((byte)charValue *
+                                     (k + 1) + DateTime.Now.Ticks));
+                }
+                else
+                {
+                    randomChar = new Random((int)((byte)charValue *
+                                     (k + 1) + DateTime.UtcNow.Ticks));
+                    k -= 1;
+                }
+            }
+
+            return stringBuilder.ToString().RandomStringSort();
+        }
+
+        private static string colorCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        private static string GetColorCharacter(int blue, int green, int red)
+        {
+            string colorChar = String.Empty;
+            int intensity = (blue + green + red) / 3 *
+                            (colorCharacters.Length - 1) / 255;
+
+            colorChar = colorCharacters.Substring(intensity, 1).ToUpper();
+            colorChar += colorChar.ToLower();
+
+            return colorChar;
+        }
+
+        public static Bitmap ScaleBitmap(this Bitmap sourceBitmap, float factor)
+        {
+            Bitmap resultBitmap = new Bitmap((int)(sourceBitmap.Width * factor),
+                                             (int)(sourceBitmap.Height * factor),
+                                             PixelFormat.Format32bppArgb);
+
+            Graphics graphics = Graphics.FromImage(resultBitmap);
+
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            graphics.DrawImage(sourceBitmap,
+                new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
+                new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
+                                                         GraphicsUnit.Pixel);
+
+            return resultBitmap;
+        }
+    }
+}
---
+++ ArtScii/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ArtScii
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Bitmap originalBitmap = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopyToClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            txtAscii.Paste();
+        }
+
+        private void btnOpenOriginal_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            ofd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                originalBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+
+                // What is the WPF equivalent? Hmmm...
+                //ApplyFilter();
+            }
+        }
+
+        private void btnSaveNewImage_Click(object sender, RoutedEventArgs e)
+        {
+            //if (string.IsNullOrEmpty(new TextRange(txtAscii.Document.ContentStart, txtAscii.Document.ContentEnd).Text))
+            //{
+            //    return;
+            //}
+
+            //Font textFont = new System.Drawing.Font(txtAscii.Document.FontFamily.ToString(), (float)numFontSize.Value, txtAscii.Font.Style);
+            //Bitmap textBitmap = originalBitmap.ASCIIFilter((int)numPixelsToChar.Value).TextToImage(textFont, (float)(numZoom.Value / 100));
+
+            //if (textBitmap != null)
+            //{
+            //    SaveFileDialog sfd = new SaveFileDialog();
+            //    sfd.Title = "Specify a file name and file path";
+            //    sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            //    sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            //    if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+            //        ImageFormat imgFormat = ImageFormat.Png;
+
+            //        if (fileExtension == "BMP")
+            //        {
+            //            imgFormat = ImageFormat.Bmp;
+            //        }
+            //        else if (fileExtension == "JPG")
+            //        {
+            //            imgFormat = ImageFormat.Jpeg;
+            //        }
+
+            //        StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+            //        textBitmap.Save(streamWriter.BaseStream, imgFormat);
+            //        streamWriter.Flush();
+            //        streamWriter.Close();
+
+            //        textBitmap.Dispose();
+            //    }
+            //}
+        }
+    }
+}
